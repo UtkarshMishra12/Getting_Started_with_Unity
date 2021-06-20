126f8c75a8838d7a30d625b025d4f884f70443b5
Utkarsh Mishra <nasautkarsh@gmail.com>
2021-06-20 22:26:15 +0530

    CH-5 (UNIT-4  UNITY)

---
+++ SoccerGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRB;
+    [SerializeField]
+    private float speed = 10.0f;
+    private GameObject focalPoint;
+    public bool hasPowerUp=false;
+    private float powerUpStrength = 10.0f;
+    public GameObject powerUpIndicator;
+    private AudioSource playerAudio;
+    public AudioClip powerSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("FocalPoint");
+        playerAudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        playerRB.AddForce(focalPoint.transform.forward * speed * forwardInput);
+        powerUpIndicator.transform.position = transform.position + new Vector3(0, -0.2f, 0);
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PowerUp"))
+        {
+            hasPowerUp = true;
+            playerAudio.PlayOneShot(powerSound);
+            powerUpIndicator.gameObject.SetActive(true);
+            Destroy(other.gameObject);
+            StartCoroutine(countdownTimer());
+        }
+    }
+
+    IEnumerator countdownTimer()
+    {
+        yield return new WaitForSeconds(7.0f);
+        hasPowerUp = false;
+        powerUpIndicator.gameObject.SetActive(false);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy") && hasPowerUp)
+        {
+            Rigidbody enemyRB = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+            enemyRB.AddForce(awayFromPlayer * powerUpStrength, ForceMode.Impulse);
+        }
+    }
+}
---
+++ SoccerGame/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemy;
+    private float startdelay = 2.0f;
+    private float spawndelay = 2.5f;
+    public int enemyCount = 0;
+    public int waveNumber = 1;
+    public GameObject powerUp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //InvokeRepeating("spawnRandomEnemy", startdelay, spawndelay);
+        spawnEnemyWave(waveNumber);
+        Instantiate(powerUp, spawnPos(), powerUp.transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+        if (enemyCount == 0)
+        {
+            waveNumber++;
+            spawnEnemyWave(waveNumber);
+            Instantiate(powerUp, spawnPos(), powerUp.transform.rotation);
+        }
+    }
+
+    private Vector3 spawnPos()
+    {
+        float spawnX = Random.Range(-9, 9);
+        float spawnZ = Random.Range(-9, 9);
+        Vector3 spawnpos = new Vector3(spawnX, 0, spawnZ);
+        return spawnpos;
+    }
+
+    void spawnEnemyWave(int enemiesToSpawn)
+    {
+
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemy, spawnPos(), enemy.transform.rotation);
+        }
+    }
+}
