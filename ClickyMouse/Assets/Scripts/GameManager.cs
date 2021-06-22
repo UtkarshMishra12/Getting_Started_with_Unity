d2a74af789f9a44d02a751193c2cf50a4970c2c2
Utkarsh Mishra <nasautkarsh@gmail.com>
2021-06-22 22:42:01 +0530

    CH-6 (UNIT-5 UNITY)

---
+++ ClickyMouse/Assets/Scripts/GameManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    public float spawnRate = 1.0f;
+    public TextMeshProUGUI scoreText;
+    private int score;
+    public TextMeshProUGUI gameOverText;
+    public bool isGameActive = false;
+    public Button restartButton;
+    public GameObject titleScreen;
+    private AudioSource explosionAudio;
+    public AudioClip audioE;
+    // Start is called before the first frame update
+    void Start()
+    {
+        explosionAudio = GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlaySound()
+    {
+        explosionAudio.PlayOneShot(audioE);
+    }
+    IEnumerator SpawnTarget()
+    {
+        while (isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+            //UpdateScore(5);
+        }
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score:" + score;
+    }
+
+    public void GameOver()
+    {
+        restartButton.gameObject.SetActive(true);
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void StartGame(int difficulty)
+    {
+        isGameActive = true;
+        spawnRate /=  difficulty;
+        score = 0;
+        StartCoroutine("SpawnTarget");
+        UpdateScore(0);
+        titleScreen.gameObject.SetActive(false);
+    }
+}
---
+++ ClickyMouse/Assets/Scripts/Target.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody targetRB;
+    private float minSpeed =12;
+    private float maxSpeed = 15;
+    private float maxTorque = 10;
+    private float xRange = 4;
+    private float yRange = -2;
+    public int pointValue;
+    private GameManager gameManager;
+    public ParticleSystem explosionPrefab;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRB = GetComponent<Rigidbody>();
+
+        targetRB.AddForce(RandomForce(), ForceMode.Impulse);
+        targetRB.AddTorque(RandomTorque(),RandomTorque(), RandomTorque(), ForceMode.Impulse);
+
+        transform.position = RandomPos();
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+
+    private void OnMouseDown()
+    {
+        if (gameManager.isGameActive)
+        {
+            gameManager.PlaySound();
+            Destroy(gameObject);
+            Instantiate(explosionPrefab, transform.position, explosionPrefab.transform.rotation);
+            gameManager.UpdateScore(pointValue);
+        }
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        if (gameObject.CompareTag("Good"))
+        {
+            gameManager.GameOver();
+        }
+    }
+
+    Vector3 RandomForce()
+    {
+        return Vector3.up* Random.Range(minSpeed,maxSpeed);
+    }
+
+    float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    Vector3 RandomPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), yRange);
+    }
+}
